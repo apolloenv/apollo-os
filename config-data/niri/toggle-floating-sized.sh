c75588a39f60eca2367c5d97d21343930b455504
Manuel Kraibacher <manuel@kraibacher.com>
2026-01-14 12:51:27 +0100

    Add auto-sized floating windows (60% width, centered)
    - Added window-rule for floating windows: 60% width proportion
    - Created toggle-floating-sized.sh script
    - Modified Mod+W keybinding to use new script
    - Floating windows now automatically sized and centered
    - Script installed to ~/.config/niri/ during deployment

---
+++ config-data/niri/toggle-floating-sized.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#####################################################################
+# Apollo OS - Toggle Floating with Auto-Size
+# Copyright © 2025 by Manuel Kraibacher
+#
+# Description: Schaltet Fenster auf Floating um
+#              Größe wird über window-rule definiert (siehe config.kdl)
+#####################################################################
+
+# Simply toggle floating - size is handled by window rules
+niri msg action toggle-window-floating
+
+
